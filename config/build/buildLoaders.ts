0689c092fbab0d6a3aba44c2d6d2082563597f8c
ZaoBaoo <yurkin.daniil96@gmail.com>
2024-06-24 00:55:13 +0500

    Добавил в индексацию папку /config/build

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,35 @@
+import { ModuleOptions } from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BuildOptions } from "./types/types";
+
+export function buildLoaders(options: BuildOptions): ModuleOptions["rules"] {
+  const isDev = options.mode === "development";
+
+  const cssLoaderWithModules = {
+    loader: "css-loader",
+    options: {
+      modules: {
+        localIdentName: isDev ? "[path][name]__[local]" : "[hash:base64:8]",
+      },
+    },
+  };
+
+  const scssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      // Translates CSS into CommonJS
+      cssLoaderWithModules,
+      // Compiles Sass to CSS
+      "sass-loader",
+    ],
+  };
+
+  const tsLoader = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
+
+  return [scssLoader, tsLoader];
+}
